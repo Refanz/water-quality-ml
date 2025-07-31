bab4a14377b4b0146aeaf611484b87f4cea33181
Refanzzzz <80124360+Refanz@users.noreply.github.com>
2025-07-31 16:00:43 +0700

    Delete water_quality_web directory

--- water_quality_web/site/src/jsMain/kotlin/com/refanzzzz/waterquality/components/sections/landingpage/Footer.kt
+++
@@ -1,8 +0,0 @@
-package com.refanzzzz.waterquality.components.sections.landingpage
-
-import androidx.compose.runtime.Composable
-
-@Composable
-fun Footer() {
-
-}
--- water_quality_web/site/src/jsMain/kotlin/com/refanzzzz/waterquality/util/Assets.kt
+++
@@ -1,5 +0,0 @@
-package com.refanzzzz.waterquality.util
-
-object Assets {
-    const val WATER_ICON = "/assets/water.svg"
-}
